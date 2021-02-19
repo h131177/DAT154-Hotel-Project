05c88f13773457c6f509a44914cbd63bd59d5bfb
h131177 <h131177@stud.hvl.no>
2021-02-19 16:51:57 +0100

    Lagt over klassene i nye Library

---
+++ ClassLibrary2/Reservation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    class Reservation
+    {
+        int roomNumber;
+        int reservationNumber;
+        //given time periode for the hotel room
+        //let user search for different wishes.
+    }
+}
---
+++ ClassLibrary2/Task.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task
+{
+    class Task
+    {
+        private int roomnr;
+        private String note;
+        private String status;
+
+        public Task (int roomnr, String note, String status)
+        {
+            this.roomnr = roomnr;
+            this.note = note;
+            this.status = status;
+        }
+
+    }
+}
