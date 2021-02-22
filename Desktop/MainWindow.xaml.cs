2e5d61b60c99eb4621bd7e6d988eba570ef00078
h131177 <54102543+h131177@users.noreply.github.com>
2021-02-22 19:04:54 +0100

    Lagt til det Kjetil og Magnus diskuterte ang. Desktop appen.

--- Desktop/MainWindow.xaml.cs
+++ Desktop/MainWindow.xaml.cs
@@ -24,8 +24,23 @@ namespace Desktop
         public MainWindow()
         {
             InitializeComponent();
+
+            // Tab 1: "add new reservations and delete reservations and associate a reservation with a particular room."
+            // Liste over alle reservasjoner, kan dobbeltklikke på ein reservasjon for å få opp
+            // ein editor der ein kan slette og velge rom. Dropdown list som viser ledige rom.
+            // I den editoren trenger ein 2 knapper, slett og velg rom.
+            // Trenger også liste over alle ledige rom den dagen, slik at resepsjonist kan dobbeltklikke
+            // på et rom for å booke til nye gjester, dette vil også kjøre ein metode for å beregne pris.
+            // Dobbeltklikk gir ein ny editor med felter for å reservere rom, trenger ein knapp reserver.
+
+            // Tab 2: "It should also be used when guests check in and out."
+            // Liste over reservasjoner som ikkje har sjekka inn
+            // Lister over gjester på hotellet som har sjekka inn
+            // Dobbeltklikke for å endre status? Må muligens ha egen editor her og.
+
+            // Tab 3: "This program should also be used to register room service and maintenance requests for rooms"
+            // Lister over alle Tasks, kanskje sortert på type?
+            // Felter og knapp for å legge til nye Tasks.
         }
     }
 }
-
-//Githubtest
