e6c7808a5a117ed1554771ada5a6fe09f50e9ddd
h131177 <54102543+h131177@users.noreply.github.com>
2021-02-22 16:10:21 +0100

    Opprettet Web Appen og ein liten endring i Task

--- ClassLibrary/Task.cs
+++ ClassLibrary/Task.cs
@@ -11,14 +11,16 @@ namespace Task
         private string task { get; set; }
         private string note { get; set; }
         private string status { get; set; }
+        private int eid { get; set; }
 
-        public Task (int roomnr, string type, string task, string note, string status)
+        public Task(int roomnr, string type, string task, string note, string status, int eid)
         {
             this.roomnr = roomnr;
             this.type = type;
             this.task = task;
             this.note = note;
             this.status = status;
+            this.eid = eid;
         }
 
     }
