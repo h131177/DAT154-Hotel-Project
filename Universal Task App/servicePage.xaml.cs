42a781786d6062001318cfe036c734886dacecad
Magnus <31276154+h182036@users.noreply.github.com>
2021-03-01 16:47:11 +0100

    Lagt pages til UWP

--- Universal Task App/servicePage.xaml.cs
+++ Universal Task App/servicePage.xaml.cs
@@ -13,34 +13,18 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace Universal_Task_App
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class servicePage : Page
     {
-        public MainPage()
+        public servicePage()
         {
             this.InitializeComponent();
-        }
-
-        private void cleaningButton_Click(object sender, RoutedEventArgs e)
-        {
-
-
-        }
-
-        private void maintenanceButton_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void serviceButton_Click(object sender, RoutedEventArgs e)
-        {
-
         }
     }
 }
